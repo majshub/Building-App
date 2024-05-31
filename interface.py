eac8cf1ea83731643635a4df56cd3722ef39cb78
Majid Hosini <majidjj108@gmail.com>
2024-05-31 16:33:20 +0330

    added lable for expense type dropdown

--- interface.py
+++ interface.py
@@ -10,6 +10,13 @@ def openExpenses():
     top = tk.Toplevel()
     top.title('Expenses Page')
 
+    #creating expenses dropdown list
+    expenseLable = tk.Label(top, text='Expense Type').grid(row= 0, column=0, padx=10, pady=10)
+    ExpenseTypes = ['Utility', 'Salary', 'Maintenance']
+    selectedExpenseType = tk.StringVar()
+    selectedExpenseType.set(ExpenseTypes[0])
+    expenseTypeList = tk.OptionMenu(top, selectedExpenseType, *ExpenseTypes).grid(row= 0, column=1, columnspan=3, padx=10, pady=10)
+
     
 
 
